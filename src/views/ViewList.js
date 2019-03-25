c2de6d5f618afee5ac08b6451e4e0f3e92ac87e6
niyorn <c.niyornram@hotmail.com>
2019-03-25 17:11:15 +0100

    add: Filter function

--- src/views/ViewList.js
+++ src/views/ViewList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 const styles = {
@@ -23,7 +23,11 @@ class ViewList extends Component {
 			api: this.props.location.state.api,
 			category: this.props.match.params.category,
 			items: []
-    }
+		}
+
+	  // This binding is necessary to make `this` work in the callback
+		this.reOrder.old = this.reOrder.old.bind(this)
+		this.reOrder.new = this.reOrder.new.bind(this)
 	}
 	
 	api = {
@@ -40,16 +44,36 @@ class ViewList extends Component {
 		})
 	}
 
+	//rearranged items based on the oldest first or the newest first
+	reOrder = {
+		new() {
+			const data = this.state.items.sort((a, b)=> {
+				return new Date(b.created) - new Date(a.created);
+			})
+			this.setState({
+				items: data
+			})
+		},
+		old() {
+			const data = this.state.items.sort((a, b)=> {
+				return new Date(a.created) - new Date(b.created);
+			})
+			this.setState({
+				items: data
+			})
+		}
+	}
+
   render() {
     return (
       <article className="container">
           <h1>{this.state.category}</h1>
-
-					<button>Date</button>
+	
+					<button onClick={this.reOrder.new} className="button">newest First</button>
+					<button onClick={this.reOrder.old} className="button">Oldest First</button>
 
 					<div style={styles['container-items']}>
 						{this.state.items.map((value,index)=> {
-							console.log(value)
 							return <section key={index} style={styles['item']}>
 								<p><span>title: </span> <span>{value.title}</span></p>
 								<p><span>name: </span> <span>{value.name}</span></p>
