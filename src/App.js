971804afc8e4e260547cbc2a2afad8dd8082a987
niyorn <c.niyornram@hotmail.com>
2019-03-25 17:10:17 +0100

    update: Error path

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import  {BrowserRouter, Route} from 'react-router-dom'
+import  {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import ViewCategory from './views/ViewCategory'
 import ViewList from './views/ViewList'
+import ViewError from './views/ViewError'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Route path="/" exact component={ViewCategory}/>
-          <Route path="/list/:category" component={ViewList}/>
+          <Switch>
+            <Route path="/" exact component={ViewCategory}/>
+            <Route path="/list/:category" component={ViewList}/>
+            <Route component={ViewError}/>
+          </Switch>
       </div>
       </BrowserRouter>
     );
